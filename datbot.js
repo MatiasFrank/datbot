e5924f1e56df67c9c107d288b4580d8ebc404c16
Peter Leth <petergl@protonmail.com>
2017-10-26 13:45:36 +0200

    Unfinished implementation of !play

--- datbot.js
+++ datbot.js
@@ -3,22 +3,21 @@
 */
 
 // Link to GitHub repo
-const link = 'https://github.com/kuff/datbot'
+const github = 'https://github.com/kuff/datbot'
 
 // Import the discord.js module
 const Discord = require('discord.js');
-
-const jsonSize = require('json-size');
-
 // Import json files
 const config = require('./settings.json');
 const quotes = require('./quotes.json');
+// Import ytdl to enable youtube playback
+const ytdl = require('ytdl-core');
 
 // Create an instance of a Discord client
 const client = new Discord.Client();
 
 // The token of your bot - https://discordapp.com/developers/applications/me
-const token = config.token
+const token = config.token;
 
 // The ready event is vital, it means that your bot will only start reacting to information
 // from Discord _after_ ready is emitted
@@ -50,19 +49,40 @@ client.on('message', message => {
         case '!github': //(Because there are no break statements)
         case '!source':
             // Send a link to the source code
-            message.channel.send('Thats right! You\'re welcome to add features to the bot, ' + message.author + ' - ' + link);
+            message.channel.send('Thats right! You\'re welcome to add features to the bot, ' + message.author + ' - ' + github);
             break;
 
         case '!ermin':
-            // Quote the man himself
+            // Get a random quote by the man himself
             const random_choice = Math.ceil(Math.random()*quotes.ermin.amount)
             message.channel.send(`${ermin} ${quotes.ermin[random_choice]} ${ermin}`)
             break;
     }
 
+    // Request a specific quote
     if (message.content.substring(0,6) === "!ermin" && message.content.length > 7) {
         const quote_index = parseInt(message.content.substring(7,8))
         message.channel.send(`${ermin} ${quotes.ermin[quote_index]} ${ermin}`)
+    }
+
+    // Playback songrequest
+    if (message.content.substring(0,5) === "!play") {
+        if (message.content.length > 5) {
+            const link = message.content.substring(6);
+            const streamOptions = { seek: 0, volume: 1 };
+            const broadcast = client.createVoiceBroadcast();
+        
+            message.member.voiceChannel.join().then(connection => {
+                const stream = ytdl(link, { filter: 'audioonly' });
+                broadcast.playStream(stream);
+                const dispatcher = connection.playBroadcast(broadcast);
+                // Find a way to leave voice chat once breadcast has ended
+            })
+            .catch(console.error);
+        }
+        else {
+            // Say something
+        }
     }
 });
 
