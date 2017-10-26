fbbbd408cbaf550a9494be7cfe634f55a24e34aa
Peter Leth <petergl@protonmail.com>
2017-10-26 21:18:26 +0200

    Added proper !help table

--- datbot.js
+++ datbot.js
@@ -26,6 +26,17 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
+// React to a given message with a given emote. If a limit greater than 1 is specified,
+// previous messages in the thread will also recieve a reaction
+function react (message, emote, limit) {
+    message.channel.fetchMessages({limit: limit})
+        .then((result) => {
+            result.forEach(message => {
+                message.react(emote);
+            })
+        });
+}
+
 // Create an event listener for messages
 client.on('message', message => {
     // Defining emotes here
@@ -47,7 +58,41 @@ client.on('message', message => {
             // Send a pm with a table of useful commands 
             // (find out how to format a table of useful commands)
             message.channel.send('Sent help!')
-            message.author.sendMessage('Hope I\'m helping!');
+            message.author.sendMessage({embed: {
+                color: 3447003,
+                author: {
+                    name: "Help has arrived!",
+                    icon_url: client.user.avatarURL
+                },
+                title: "GitHub repo",
+                url: link,
+                description: "Feel free to suggest features or fork and make a pull request!",
+                fields: [{
+                    name: "!ping",
+                    value: "Test the bot in selected channel. Retrives \"pong\"."
+                },
+                {
+                    name: "!help",
+                    value: "The command you just typed! Learn about all available commands.",
+                },
+                {
+                    name: "!code, !github, !source",
+                    value: "Retrives a link to the github repo."
+                },
+                {
+                    name: "!ermin",
+                    value: "Retrives a random Ermin quote."
+                },
+                {
+                    name: "!ermin { Integer }",
+                    value: "Retrives a specific Ermin quote, specified by an integer value."
+                }],
+                timestamp: new Date(),
+                footer: {
+                    icon_url: client.user.avatarURL,
+                    text: "!help @ DatBot :*"
+                }
+            }});
             break;
 
         case '!code': // Nesting cases creates synonyms!
@@ -83,15 +128,17 @@ client.on('message', message => {
         
         case '!react':
             // React with Ermin's face on the last five messages posted
-            console.log("Reacted!");
-            message.channel.fetchMessages({limit: 5})
-                .then((result) => {
-                    result.forEach(message => {
-                        message.react(ermin);
-                    })
-                });
+            react(message, ermin, 5);
             break;
     }
+
+    words.find((elem) => {
+        if (elem === "incest" || elem === "søster") {
+            // If any sentence mentions "incest" or "søster", react with Ermin's face
+            react(message, ermin, 1);
+        }
+        // Add handlers for other mentions here
+    });
 });
 
 // Log our bot in
