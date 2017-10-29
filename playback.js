39ab0b3c54ada2adca879e594b37f12c003f6527
Peter Leth <petergl@protonmail.com>
2017-10-29 09:11:41 +0100

    Added !skip

--- playback.js
+++ playback.js
@@ -1,6 +1,15 @@
 const ytdl = require('ytdl-core');
 
+/**
+ * Homemade module for handling streaming and playback of 
+ * YouTube audio through Discord.js
+ */
 module.exports = class Playback {
+
+    /**
+     * @param {*} client 
+     * @param {*} streamOptions 
+     */
     constructor(client, streamOptions = { seek: 0, volume: 1 }) {
         this.client = client;
         this.streamOptions = streamOptions;
@@ -8,46 +17,65 @@ module.exports = class Playback {
         this.playing = false;
     }
 
+    /**
+     * Terminates voice connection
+     */
     end() {
         this.connection.disconnect();
         this.playing = false;
     }
 
+    /**
+     * Plays next song in queue
+     */
     play() {
-        const link = this.playlist.pop();
+        // Get next link in queue
+        const link = this.playlist.shift();
+        console.log("Playlist link: " + link);
+        console.log("Playlist play: " + this.playlist);        
 
+        // Initiate playback
         this.stream = ytdl(link, { filter: 'audioonly' });
         const bc = this.client.createVoiceBroadcast();
         this.broadcast = bc.playStream(this.stream);
-        this.connection.playBroadcast(this.broadcast);
+        this.dispatcher = this.connection.playBroadcast(this.broadcast);
 
         console.log("Playing music!");
 
-        this.broadcast.once('end', () => {
+        this.dispatcher.once('end', () => {
+            // When the song ends either play next in playlist 
             if (this.playlist.length > 0) {
+                bc.end();
                 this.play();
             }
+            // or terminate voice connection
             else {
                 this.end();
             }
         });
     }
 
+    /**
+     * Verifies given link and plays it
+     * @param {*} link 
+     * @param {*} message 
+     */
     queue(link, message) {
-        // Verify given link and play it
-
         // Verify using ytdl?
 
         // Add the song to the playlist
-        this.playlist.unshift(link);        
+        this.playlist.push(link);
+        console.log("Playlist queue: " + this.playlist);
 
         if (this.playing) {
+            // If we're already playing, tell the user 
+            // that their song request was added to the queue
             message.channel.send("Added link to !queue, " + message.author);
         }
         else {
+            // if not, join voice channel and start jamming!
             message.member.voiceChannel.join()
                 .then(connection => {
-                    // Join voice server and start jamming!
                     this.connection = connection;
                     this.playing = true;
                     this.play();
@@ -55,29 +83,41 @@ module.exports = class Playback {
         }
     }
 
+    /**
+     * Pauses playback
+     */
     pause() {
-        // Pause playback
         this.stream.pause();
         this.broadcast.pause();
     }
 
+    /**
+     * Resumes playback
+     */
     resume() {
-        // Resume playback
         this.stream.pause();
         this.broadcast.resume();
     }
 
+    /**
+     * Skips the current song being played
+     */
     skip() {
-        // Skip the current song being played
-        // this.broadcast.end();
-        this.play();
+        this.dispatcher.end();
     }
 
+    /**
+     * Adjusts volume to given parameter
+     * @param {*} val 
+     */
     setVolume(val) {
-        // Adjust volume to given parameter
+        // Code goes here...
     }
 
+    /**
+     * Returns volume value
+     */
     getVolume() {
-        // Return volume value
+        // Code goes here...
     }
 }
